d401c87faad54f01a60eeec0a3ec681c61571e5d
Kristen Shields <shieldskristen0@gmail.com>
2025-01-18 10:02:26 -0700

    updated server file and added schema files

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,3 +29,5 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+startApolloServer();
